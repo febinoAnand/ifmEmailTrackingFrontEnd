6fd7dc5e4d71cd8c8086da6a5ae451bbd5033d34
karthik <karthik202m@gmail.com>
2024-05-04 20:08:05 +0530

    Added Data for table in send report smsgateway

--- src/views/base/smsgateway/sendreport.js
+++ src/views/base/smsgateway/sendreport.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 import axios from 'axios';
-import { cilMediaSkipForward, cilFilter, cilMagnifyingGlass } from '@coreui/icons';
-
 import {
     CButton,
     CCard,
@@ -19,64 +17,72 @@ import {
     CTableHead,
     CTableHeaderCell,
     CTableRow,
-  } from '@coreui/react'
-  import CIcon from '@coreui/icons-react';
+} from '@coreui/react';
 import BaseURL from 'src/assets/contants/BaseURL';
 
-class SendReport extends React.Component{
+class SendReport extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            reports: []
+        };
+    }
 
+    componentDidMount() {
+        axios.get(BaseURL+'smsgateway/sendreport/')
+            .then(response => {
+                this.setState({ reports: response.data });
+            })
+            .catch(error => {
+                console.error('Error fetching reports:', error);
+            });
+    }
 
+    render() {
+        const { reports } = this.state;
 
-  
- render(){ 
-  return (
-    <>
-     <CRow>
-
-      <CCol xs={12}>
-        <CCard className="mb-4">
-          <CCardHeader>
-            <strong>SEND REPORT</strong>
-          </CCardHeader>
-          <CCardBody>
-            
-            
-            
-              <CTable striped hover>
-                <CTableHead>
-                  <CTableRow>
-                    <CTableHeaderCell scope="col">Si.No</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Date</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Time</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">To Number</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">From Number</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Message</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Delivery Status</CTableHeaderCell>
-                    
-                  </CTableRow>
-                </CTableHead>
-                <CTableBody>
-                      <CTableRow >
-                        <CTableHeaderCell></CTableHeaderCell>
-                        <CTableDataCell></CTableDataCell>
-                        <CTableDataCell></CTableDataCell>
-                        <CTableDataCell></CTableDataCell>
-                        <CTableDataCell></CTableDataCell>
-                        <CTableDataCell></CTableDataCell>
-                        <CTableDataCell></CTableDataCell>
-                      </CTableRow>
-                  </CTableBody>
-              </CTable>
-            
-          </CCardBody>
-        </CCard>
-      </CCol>
-      
-      </CRow>
-
-     </>
-  )
-            }
+        return (
+            <>
+                <CRow>
+                    <CCol xs={12}>
+                        <CCard className="mb-4">
+                            <CCardHeader>
+                                <strong>SEND REPORT</strong>
+                            </CCardHeader>
+                            <CCardBody>
+                                <CTable striped hover>
+                                    <CTableHead>
+                                        <CTableRow>
+                                            <CTableHeaderCell scope="col">Si.No</CTableHeaderCell>
+                                            <CTableHeaderCell scope="col">Date</CTableHeaderCell>
+                                            <CTableHeaderCell scope="col">Time</CTableHeaderCell>
+                                            <CTableHeaderCell scope="col">To Number</CTableHeaderCell>
+                                            <CTableHeaderCell scope="col">From Number</CTableHeaderCell>
+                                            <CTableHeaderCell scope="col">Message</CTableHeaderCell>
+                                            <CTableHeaderCell scope="col">Delivery Status</CTableHeaderCell>
+                                        </CTableRow>
+                                    </CTableHead>
+                                    <CTableBody>
+                                        {reports.map((report, index) => (
+                                            <CTableRow key={index}>
+                                                <CTableDataCell>{index + 1}</CTableDataCell>
+                                                <CTableDataCell>{report.date}</CTableDataCell>
+                                                <CTableDataCell>{report.time}</CTableDataCell>
+                                                <CTableDataCell>{report.toNumber}</CTableDataCell>
+                                                <CTableDataCell>{report.fromNumber}</CTableDataCell>
+                                                <CTableDataCell>{report.message}</CTableDataCell>
+                                                <CTableDataCell>{report.deliveryStatus}</CTableDataCell>
+                                            </CTableRow>
+                                        ))}
+                                    </CTableBody>
+                                </CTable>
+                            </CCardBody>
+                        </CCard>
+                    </CCol>
+                </CRow>
+            </>
+        );
+    }
 }
 
-export default SendReport
+export default SendReport;
--- src/views/base/smsgateway/settings.js
+++ src/views/base/smsgateway/settings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import {
@@ -16,12 +16,73 @@ import {
 import BaseURL from 'src/assets/contants/BaseURL';
 
 class Settings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sid: '',
+            authToken: '',
+            number: '',
+        };
+    }
+
+    componentDidMount() {
+        this.fetchSettings();
+    }
+
+    fetchSettings() {
+        axios.get(BaseURL + 'smsgateway/setting/')
+            .then(response => {
+                const { sid, authToken, number } = response.data;
+    
+                this.setState({ 
+                    sid, 
+                    authToken, 
+                    number
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching settings:', error);
+            });
+    }
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = this.validate();
+        if (Object.keys(errors).length === 0) {
+            console.log('Submitted data:', this.state);
+
+            const {sid, authToken, number } = this.state;
+            const data = {
+                sid,
+                authToken,
+                number
+            };
+
+            axios.put(BaseURL + 'smsgateway/setting/', data)
+            .then(() => {
+                this.setState({ successMessage: 'Settings updated successfully', errors: {} });
+            })
+            .catch(error => {
+                console.error('Error updating settings:', error);
+                toast.error('Failed to update settings');
+            });
+        } else {
+            this.setState({ errors });
+            console.log('Validation errors:', errors);
+        }
+    };
 
     render() {
+        const { sid, authToken, number } = this.state;
 
         return (
             <>
-            
                 <CRow>
                     <CCol xs={12}>
                         <CCard className="mb-4">
@@ -33,19 +94,19 @@ class Settings extends React.Component {
                                     <CRow className="mb-3">
                                         <CFormLabel htmlFor="host" className="col-sm-2 col-form-label">SID</CFormLabel>
                                         <CCol sm={10}>
-                                            <CFormInput type="text" id="host" name="host"/>
+                                            <CFormInput type="text" id="sid" name="sid" value={sid} onChange={this.handleChange} />
                                         </CCol>
                                     </CRow>
                                     <CRow className="mb-3">
                                         <CFormLabel htmlFor="port" className="col-sm-2 col-form-label">Auth-Token</CFormLabel>
                                         <CCol sm={10}>
-                                            <CFormInput type="text" id="port" name="port"/>
+                                        <CFormInput type="password" id="authtoken" name="authToken" value={authToken} onChange={this.handleChange} />
                                         </CCol>
                                     </CRow>
                                     <CRow className="mb-3">
                                         <CFormLabel htmlFor="username" className="col-sm-2 col-form-label">Number</CFormLabel>
                                         <CCol sm={10}>
-                                            <CFormInput type="text" id="username" name="username"/>
+                                            <CFormInput type="text" id="username" name="username" value={number} onChange={this.handleChange} />
                                         </CCol>
                                     </CRow>
                                     <CRow className="justify-content-center">
