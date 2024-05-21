09dc9ed214d43fda0473791feb17f15307b769c1
karthik <karthik202m@gmail.com>
2024-05-21 15:12:26 +0530

    Corrected the ticket report page to get data in json formate

--- src/views/base/emailtracking/ticketreport.js
+++ src/views/base/emailtracking/ticketreport.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 import axios from 'axios';
 
 import {
-    CCard,
-    CCardBody,
-    CCardHeader,
-    CCol,
-    CRow,
-    CTable,
-    CTableBody,
-    CTableDataCell,
-    CTableHead,
-    CTableHeaderCell,
-    CTableRow,
-  } from '@coreui/react'
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react';
 import BaseURL from 'src/assets/contants/BaseURL';
 
-class TicketReport extends React.Component{
+class TicketReport extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -39,62 +39,51 @@ class TicketReport extends React.Component{
       });
   }
 
+  render() {
+    const { tickets } = this.state;
 
-
-  
- render(){ 
-  const { tickets } = this.state;
-
-  return (
-    <>
-     <CRow>
-
-      <CCol xs={12}>
-        <CCard className="mb-4">
-          <CCardHeader>
-            <strong>TICKET TABLE</strong>
-          </CCardHeader>
-          <CCardBody>
-            
-            
-            
-              <CTable striped hover>
-                <CTableHead>
-                  <CTableRow>
-                    <CTableHeaderCell scope="col">Si.No</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Ticket Name</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Date</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Time</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Message</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Extracted json</CTableHeaderCell>
-                    <CTableHeaderCell scope="col">Actual json</CTableHeaderCell>
-                    
-                  </CTableRow>
-                </CTableHead>
-                <CTableBody>
-                {tickets.map((ticket, index) => (
-                  <CTableRow key={index}>
-                    <CTableHeaderCell>{index + 1}</CTableHeaderCell>
-                    <CTableDataCell>{ticket.ticketname}</CTableDataCell>
-                    <CTableDataCell>{ticket.date}</CTableDataCell>
-                    <CTableDataCell>{ticket.time}</CTableDataCell>
-                    <CTableDataCell>{ticket.inboxMessage}</CTableDataCell>
-                    <CTableDataCell>{ticket.required_json}</CTableDataCell>
-                    <CTableDataCell>{ticket.actual_json}</CTableDataCell>
-                  </CTableRow>
-                ))}
-              </CTableBody>
-              </CTable>
-            
-          </CCardBody>
-        </CCard>
-      </CCol>
-      
-      </CRow>
-
-     </>
-  )
-            }
+    return (
+      <>
+        <CRow>
+          <CCol xs={12}>
+            <CCard className="mb-4">
+              <CCardHeader>
+                <strong>TICKET TABLE</strong>
+              </CCardHeader>
+              <CCardBody>
+                <CTable striped hover>
+                  <CTableHead>
+                    <CTableRow>
+                      <CTableHeaderCell scope="col">Si.No</CTableHeaderCell>
+                      <CTableHeaderCell scope="col">Ticket Name</CTableHeaderCell>
+                      <CTableHeaderCell scope="col">Date</CTableHeaderCell>
+                      <CTableHeaderCell scope="col">Time</CTableHeaderCell>
+                      <CTableHeaderCell scope="col">Message</CTableHeaderCell>
+                      <CTableHeaderCell scope="col">Extracted json</CTableHeaderCell>
+                      <CTableHeaderCell scope="col">Actual json</CTableHeaderCell>
+                    </CTableRow>
+                  </CTableHead>
+                  <CTableBody>
+                    {tickets.map((ticket, index) => (
+                      <CTableRow key={index}>
+                        <CTableHeaderCell>{index + 1}</CTableHeaderCell>
+                        <CTableDataCell>{ticket.ticketname}</CTableDataCell>
+                        <CTableDataCell>{ticket.date}</CTableDataCell>
+                        <CTableDataCell>{ticket.time}</CTableDataCell>
+                        <CTableDataCell>{ticket.inboxMessage}</CTableDataCell>
+                        <CTableDataCell>{JSON.stringify(ticket.required_json)}</CTableDataCell>
+                        <CTableDataCell>{JSON.stringify(ticket.actual_json)}</CTableDataCell>
+                      </CTableRow>
+                    ))}
+                  </CTableBody>
+                </CTable>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </>
+    );
+  }
 }
 
-export default TicketReport
+export default TicketReport;
