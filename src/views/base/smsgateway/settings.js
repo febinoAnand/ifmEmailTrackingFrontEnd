3c843f6196375ea247ab1e34db561cb9338b0cf7
karthik <karthik202m@gmail.com>
2024-06-25 19:36:50 +0530

    Added checkbox in the SMS setting page

--- src/views/base/smsgateway/settings.js
+++ src/views/base/smsgateway/settings.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {
     CButton,
     CCard,
@@ -11,6 +12,7 @@ import {
     CFormInput,
     CFormLabel,
     CRow,
+    CFormCheck
 } from '@coreui/react';
 import BaseURL from 'src/assets/contants/BaseURL';
 
@@ -18,20 +20,25 @@ class Settings extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id:'',
+            id: '',
             sid: '',
-            auth_token: ''
+            auth_token: '',
+            is_active: false,
+            successMessage: ''
         };
     }
 
     componentDidMount() {
-        axios.get(BaseURL+'smsgateway/setting/')
+        this.fetchSettings();
+    }
+
+    fetchSettings = () => {
+        axios.get(BaseURL + 'smsgateway/setting/')
             .then(response => {
                 const settings = response.data[0];
                 if (settings) {
-                    const { id, sid, auth_token } = settings;
-                    console.log(settings);
-                    this.setState({ id, sid, auth_token });
+                    const { id, sid, auth_token, is_active } = settings;
+                    this.setState({ id, sid, auth_token, is_active });
                 } else {
                     console.error('Empty response data.');
                     toast.error('Failed to fetch settings: Empty response data.');
@@ -41,29 +48,36 @@ class Settings extends Component {
                 console.error('Error fetching settings:', error);
                 toast.error('Failed to fetch settings.');
             });
-    }     
+    }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
-        
-        const { id, sid, auth_token } = this.state;
-    
-        axios.put(`${BaseURL}smsgateway/setting/${id}/`, { sid, auth_token })
-            .then(response => {
-                console.log(response);
-                toast.success('Settings updated successfully!');
-            })
-            .catch(error => {
-                console.error('Error updating settings:', error);
-                toast.error('Failed to update settings.');
-            });
-    }    
+
+        const { id, sid, auth_token, is_active } = this.state;
+
+        try {
+            const response = await axios.put(`${BaseURL}smsgateway/setting/${id}/`, { sid, auth_token, is_active });
+            console.log(response);
+            this.setState({ successMessage: 'Settings updated successfully!' });
+            setTimeout(() => {
+                this.setState({ successMessage: '' });
+            }, 5000);
+        } catch (error) {
+            console.error('Error updating settings:', error);
+            toast.error('Failed to update settings.');
+        }
+    }
 
     render() {
-        const { sid, auth_token } = this.state;
+        const { sid, auth_token, is_active, successMessage } = this.state;
 
         return (
             <CRow>
+                {successMessage && ( 
+                    <div className="alert alert-success" role="alert">
+                        {successMessage}
+                    </div>
+                )}
                 <CCol xs={12}>
                     <CCard className="mb-4">
                         <CCardHeader>
@@ -81,8 +95,19 @@ class Settings extends Component {
                                 <CRow className="mb-3">
                                     <CFormLabel htmlFor="authToken" className="col-sm-2 col-form-label">Auth-Token</CFormLabel>
                                     <CCol sm={10}>
-                                        <CFormInput type="text" id="authToken" name="authToken" value={auth_token} onChange={(e) => this.setState({ authToken: e.target.value })} />
+                                        <CFormInput type="text" id="authToken" name="authToken" value={auth_token} onChange={(e) => this.setState({ auth_token: e.target.value })} />
                                         <div className="form-text" style={{ fontSize: 12 }}>* Note : Auth Token is required</div>
+                                    </CCol>
+                                </CRow>
+                                <CRow className="mb-3">
+                                    <CCol sm="10" className="offset-sm-2">
+                                        <CFormCheck
+                                            id="isActive"
+                                            name="isActive"
+                                            label="Is Active"
+                                            checked={is_active}
+                                            onChange={(e) => this.setState({ is_active: e.target.checked })}
+                                        />
                                     </CCol>
                                 </CRow>
                                 <CRow className="justify-content-center">
